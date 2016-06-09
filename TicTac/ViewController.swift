edefd49fc4c2b56a5cc25a3a1de5dffabe2ec688
libranner <libranner@gmail.com>
2016-06-09 15:36:17 -0400

    Initial Commit

---
+++ TicTac/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TicTac
+//
+//  Created by Libranner Leonel Santos Espinal on 6/9/16.
+//  Copyright © 2016 Libranner Leonel Santos Espinal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
