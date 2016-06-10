3d095470ba1447627c83972473ffc471e4e21dea
libranner <libranner@gmail.com>
2016-06-10 00:13:44 -0400

    Moved logic from Game to Brain Class

--- TicTac/Game.swift
+++ TicTac/Game.swift
@@ -9,51 +9,22 @@
 import UIKit
 
 class Game: NSObject {
-    var board = [[String]](count: 3, repeatedValue:[String](count: 3, repeatedValue:String()))
+    let columns  = 3
+    let rows = 3
+    let brain: Brain
     
     override init() {
+        self.brain = Brain(centerCoordinate: Coordinate(1,1))
         super.init()
-        start()
+        start(columns, rows: rows)
     }
     
-    func start(){
-        self.board = [[String]](count: 3, repeatedValue:[String](count: 3, repeatedValue:String()))
+    func start(columns: Int, rows: Int){
+        self.brain.board = [[State]](count: columns, repeatedValue:[State](count: rows, repeatedValue:State.Blank))
     }
     
-    func play(isFirstPlayer: Bool, coordinate: Coordinate){
-        self.board[coordinate.x][coordinate.y] = (isFirstPlayer) ? "X" : "O"
+    func move(state:State, coordinate: Coordinate) {
+        self.brain.play(state, coordinate: coordinate)
     }
     
-    func getValueOnCoordinate(coordinate: Coordinate) throws -> String{
-        if checkCoordinate(coordinate) {
-            return self.board[coordinate.x][coordinate.y]
-        }
-        throw GameError.IndexOutOfRange
-    }
-    
-    func checkCoordinate(coordinate: Coordinate) -> Bool {
-        return (coordinate.x >= 0 && coordinate.x < 3 && coordinate.y >= 0 && coordinate.y < 3)
-    }
-    
-    func isGameOver() -> Bool{
-        var hasWon = false
-        for index in 0...2 {
-            var row = self.board[0]
-            var row3 = self.board[1]
-            var row2 = self.board[2]
-            
-            
-            
-            if(row[0] == row[1] && row[0] == row[2]){
-                hasWon = true
-                break
-            }
-        }
-        
-        if(!hasWon){
-            
-        }
-        
-        return true
-    }
 }
