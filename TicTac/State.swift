6b918c89f2f088237d95f9b42a6324e05adfc442
libranner <libranner@gmail.com>
2016-06-09 23:56:04 -0400

    Create support class for states of the boxes on the game board

---
+++ TicTac/State.swift
@@ -0,0 +1,15 @@
+//
+//  State.swift
+//  TicTac
+//
+//  Created by Libranner Leonel Santos Espinal on 6/9/16.
+//  Copyright © 2016 Libranner Leonel Santos Espinal. All rights reserved.
+//
+
+import UIKit
+
+enum State {
+    case Blank
+    case X
+    case O
+}
